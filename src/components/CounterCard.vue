20bc1b72dce35a9083a86394434c24580ffc88e1
LODOWNTHROWDOWN <martinlauren06@gmail.com>
2022-07-22 16:11:43 -0400

    Combined work

---
+++ src/components/CounterCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="pb-3">
+    <filter-label label="Quantity"></filter-label>
+    <div class="pt-5">
+      <div class="flex place-content-center border py-2 px-4 rounded-full ">
+        <div class="flex items-center justify-center gap-x-4">
+          <button class="px-4 py-2"
+                  @click="increaseCounter">+
+          </button>
+          {{ count }}
+          <button class="px-4 py-2"
+                  @click="decreaseCounter">-
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FilterLabel from "./FilterLabel.vue";
+
+export default {
+  components: {FilterLabel},
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    increaseCounter() {
+      this.count++
+    },
+    decreaseCounter() {
+      this.count--
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+/*.qy{
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100%;
+  text-align: center;
+
+}*/
+button {
+  width: 90px;
+  height: 60px;
+  font-size: 20px;
+}
+
+
+</style>
---
+++ src/components/SizeCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="py-5">
+    <filter-label label="Sizes" class="pb-3"></filter-label>
+    <div class="flex border-2 rounded-full border-light w-fc">
+      <div>
+        <button
+            class="px-3 py-3 uppercase border-r-2 cursor-pointer last:border-r-0 last:rounded-r-full first:rounded-l-full border-light sizeOption"
+            v-for="size in sizes"
+            :key="size"
+            @click="updateAttributes('13', size.id)">
+          <h2>{{ size.name }}</h2>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FilterLabel from "./FilterLabel.vue";
+export default {
+  name: "size-card",
+  components: {FilterLabel},
+  props: ['sizes', 'updateAttributes']
+}
+</script>
+
+<style scoped>
+
+</style>
